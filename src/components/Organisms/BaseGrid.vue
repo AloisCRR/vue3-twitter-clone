fa5152c3b31d2e3175d3157667e511cddf236d9b
Alois Carrera <aloiscarrera@gmail.com>
2020-10-06 13:11:32 -0500

    Base profile layout

--- src/components/Organisms/BaseGrid.vue
+++ src/components/Organisms/BaseGrid.vue
@@ -1,13 +1,28 @@
 <template>
-  <div></div>
+  <div class="h-screen grid grid-cols-4 grid-rows-3">
+    <Navigation />
+    <Profile />
+    <Knows />
+    <Trending />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Navigation from "@/components/Molecules/Navigation.vue";
+import Profile from "@/components/Molecules/Profile.vue";
+import Knows from "@/components/Molecules/Knows.vue";
+import Trending from "@/components/Molecules/Trending.vue";
 
 export default defineComponent({
   name: "BaseGrid",
   props: {},
+  components: {
+    Navigation,
+    Profile,
+    Knows,
+    Trending,
+  },
 });
 </script>
 
