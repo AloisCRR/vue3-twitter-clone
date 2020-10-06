82320d9cb1ab63c6e0b0618ad815e504f59a0f4d
Alois Carrera <aloiscarrera@gmail.com>
2020-10-06 13:10:43 -0500

    Nav list structure

--- src/components/Molecules/Navigation.vue
+++ src/components/Molecules/Navigation.vue
@@ -1,13 +1,38 @@
 <template>
-  <div></div>
+  <div class="flex flex-col row-span-3">
+    <NavButton v-for="(options, index) in nav" :key="index" :text="options" />
+    <TweetButton />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import NavButton from "@/components/Atoms/NavButton.vue";
+import TweetButton from "@/components/Atoms/TweetButton.vue";
 
 export default defineComponent({
   name: "Navigation",
   props: {},
+  components: {
+    NavButton,
+    TweetButton,
+  },
+  setup() {
+    const nav = [
+      "Inicio",
+      "Explorar",
+      "Notifiaciones",
+      "Mensajes",
+      "Guardados",
+      "Listas",
+      "Perfil",
+      "Más opciones",
+    ];
+
+    return {
+      nav,
+    };
+  },
 });
 </script>
 
