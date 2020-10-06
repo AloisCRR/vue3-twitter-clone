c26c146e7dae69f0a0a69c1dfc1e657443879fe9
Alois Carrera <aloiscarrera@gmail.com>
2020-10-06 13:13:38 -0500

    Profile page implementation

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <ProfilePage />
 </template>
 
-<style lang="scss"></style>
+<script lang="ts">
+import { defineComponent } from "vue";
+import ProfilePage from "@/components/Pages/ProfilePage.vue";
+
+export default defineComponent({
+  name: "App",
+  props: {},
+  components: {
+    ProfilePage,
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  background-color: #192734;
+}
+</style>
