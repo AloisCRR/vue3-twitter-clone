e524327344ea46752535608ea3a1cfe3cfb645f3
Alois Carrera <aloiscarrera@gmail.com>
2020-10-06 13:13:21 -0500

    Profile base grid

---
+++ src/components/Pages/ProfilePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto h-screen">
+    <BaseGrid />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseGrid from "@/components/Organisms/BaseGrid.vue";
+
+export default defineComponent({
+  name: "ProfilePage",
+  props: {},
+  components: {
+    BaseGrid,
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
