1455b7d85e472d489bcb6068319082050b1ae040
Alois Carrera <aloiscarrera@gmail.com>
2020-10-06 13:11:17 -0500

    Profile section info bar

--- src/components/Molecules/Profile.vue
+++ src/components/Molecules/Profile.vue
@@ -1,13 +1,19 @@
 <template>
-  <div></div>
+  <div class="bg-yellow-700 flex flex-col row-span-3 col-span-2">
+    <SectionName />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SectionName from "@/components/Atoms/SectionName.vue";
 
 export default defineComponent({
   name: "Profile",
   props: {},
+  components: {
+    SectionName,
+  },
 });
 </script>
 
