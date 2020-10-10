dea8faf16ad45f8dea78b1d4bff8fb5c1c578452
Alois Carrera <aloiscarrera@gmail.com>
2020-10-09 22:39:05 -0500

    Sidebar (Right)
    Changed all right column to exactly fit search component (with tailwind grid is a pain).

---
+++ src/components/Molecules/RightBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-span-3 row-span-fill-1 flex flex-col">
+    <search-bar />
+    <knows />
+    <trending />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SearchBar from "@/components/Atoms/SearchBar.vue";
+import Knows from "@/components/Molecules/Knows.vue";
+import Trending from "@/components/Molecules/Trending.vue";
+
+export default defineComponent({
+  name: "RightBar",
+  props: {},
+  components: {
+    SearchBar,
+    Knows,
+    Trending,
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
